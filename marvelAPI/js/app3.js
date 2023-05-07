c23e1aa959f6b5c274815f6e0bac365a6173b309
johan0426 <127363971+johan0426@users.noreply.github.com>
2023-05-07 18:13:39 -0500

    Add files via upload
    consumo de api

---
+++ marvelAPI/js/app3.js
@@ -0,0 +1,27 @@
+//comics
+const marvel = {
+  render: () => {
+    const urlAPI = 'https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=07ebebd3f780dca8ffa3760e7a1a41f5&hash=dcf4e728034739a335af4e6076fa0b53';
+    const container = document.querySelector('#marvel-row');
+    let contentHTML = '';
+    fetch(urlAPI)
+      .then(res => res.json())
+      .then((json) => {
+        for (const hero of json.data.results) {
+          let urlHero = hero.urls[0].url;
+          let title = hero.title;
+          console.log(hero.thumbnail.path + hero.thumbnail.extension)
+          contentHTML += `
+            <div class="col-md-4">
+                <a href="${urlHero}" target="_blank">
+                  <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+                </a>
+                <h3 class="title">${title}</h3>
+            </div>`;
+        }
+        container.innerHTML = contentHTML;
+      })
+  }
+};
+marvel.render();
+
